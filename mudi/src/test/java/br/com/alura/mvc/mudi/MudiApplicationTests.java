b96f24590fe2d535f7199ec79df41847205ce1d3
Thiago Silva <t.ferraz@nc0270.conductor.tecnologia>
2022-01-26 08:52:37 -0300

    testando primeira alteracao de commit

--- mudi/src/test/java/br/com/alura/mvc/mudi/MudiApplicationTests.java
+++ mudi/src/test/java/br/com/alura/mvc/mudi/MudiApplicationTests.java
@@ -2,7 +2,7 @@ package br.com.alura.mvc.mudi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Testando a primeira alteracao de commit
 @SpringBootTest
 class MudiApplicationTests {
 
